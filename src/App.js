77d757ef56cac6db075464629ffa1d46a04b7cc4
jeevitha527 <122962726+jeevitha527@users.noreply.github.com>
2023-08-26 10:59:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {Route, Switch, Redirect} from 'react-router-dom'
+import LoginForm from './components/LoginForm'
 import Home from './components/Home'
+import NotFound from './components/NotFound'
 import ProtectedRoute from './components/ProtectedRoute'
 import AboutJobItem from './components/AboutJobItem'
 import AllJobs from './components/AllJobs'
-import NotFound from './components/NotFound'
-import LoginForm from './components/LoginForm'
+
 import './App.css'
 
 /* These are the lists used in the application. You can move them to any component needed.
