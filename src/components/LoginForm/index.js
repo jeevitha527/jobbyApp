8587ff2873280f21b9bb71c79d4277f54a5613db
jeevitha527 <122962726+jeevitha527@users.noreply.github.com>
2023-08-29 12:14:37 +0530

    Update index.js

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -3,60 +3,60 @@ import {Redirect} from 'react-router-dom'
 import Cookies from 'js-cookie'
 import './index.css'
 
-const websiteLogoInForm =
-  'https://assets.ccbp.in/frontend/react-js/logo-img.png'
-
 class LoginForm extends Component {
-  state = {username: '', password: '', showSubmitError: false, errorMsg: ''}
+  state = {username: '', password: '', errorMsg: '', errorOccurred: false}
 
-  onGetUserName = event => this.setState({username: event.target.value})
+  onChangeUserName = event => {
+    this.setState({username: event.target.value})
+  }
 
-  onGetPassword = event => this.setState({password: event.target.value})
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
 
   onSubmitSuccess = jwtToken => {
+    Cookies.set('jwt_token', jwtToken, {expires: 1})
     const {history} = this.props
-
-    Cookies.set('jwt_token', jwtToken, {expires: 30, path: '/'})
-
     history.replace('/')
   }
 
-  onSubmitFailure = errorMsg => {
-    this.setState({showSubmitError: true, errorMsg})
+  onSubmitFail = err => {
+    this.setState({errorMsg: err, errorOccurred: true})
   }
 
-  onSubmitLoginForm = async event => {
+  onSubmitForm = async event => {
     event.preventDefault()
     const {username, password} = this.state
     const userDetails = {username, password}
-    const loginApiUrl = 'https://apis.ccbp.in/login'
+    const url = 'https://apis.ccbp.in/login'
     const options = {
       method: 'POST',
       body: JSON.stringify(userDetails),
     }
-    const response = await fetch(loginApiUrl, options)
+    const response = await fetch(url, options)
     const data = await response.json()
     if (response.ok === true) {
       this.onSubmitSuccess(data.jwt_token)
     } else {
-      this.onSubmitFailure(data.error_msg)
+      console.log(data)
+      this.onSubmitFail(data.error_msg)
     }
   }
 
   render() {
-    const {username, password, showSubmitError, errorMsg} = this.state
+    const {username, password, errorOccurred, errorMsg} = this.state
     const jwtToken = Cookies.get('jwt_token')
     if (jwtToken !== undefined) {
       return <Redirect to="/" />
     }
     return (
       <div className="login-container">
-        <form
-          className="login-form-container"
-          onSubmit={this.onSubmitLoginForm}
-        >
+        <form className="login-form-container" onSubmit={this.onSubmitForm}>
           <div className="form-logo-container">
-            <img src={websiteLogoInForm} alt="website logo" />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+              alt="website logo"
+            />
           </div>
           <label className="form-label" htmlFor="username">
             USERNAME
@@ -66,7 +66,7 @@ class LoginForm extends Component {
             className="form-input"
             type="text"
             value={username}
-            onChange={this.onGetUserName}
+            onChange={this.onChangeUserName}
             placeholder="username"
             id="username"
           />
@@ -80,7 +80,7 @@ class LoginForm extends Component {
             className="form-input"
             type="password"
             value={password}
-            onChange={this.onGetPassword}
+            onChange={this.onChangePassword}
             placeholder="password"
             id="password"
           />
@@ -89,7 +89,7 @@ class LoginForm extends Component {
           <button className="form-submit-button" type="submit">
             Login
           </button>
-          {showSubmitError && <p className="error-message">*{errorMsg}</p>}
+          {errorOccurred && <p className="error-message">*{errorMsg}</p>}
         </form>
       </div>
     )
